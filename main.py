aa3cc2b8fa1e2a620053a8db5bc6b501a57daf08
dg2026601-pixel <dg2026601@danggok.hs.kr>
2026-05-22 10:06:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 Web VPython 3.2
 import random
-ball = sphere(pos=vector(0, 0, 0), radius=0.05)
+scene = canvas(title = 'ping pong!', width = 600, height = 400)
+bar1 = box(pos=vector(-0.8, 0, 0), size=vector(0.1, 0.8, 0.5), color=color.white)
+bar2 = box(pos=vector(0.8, 0, 0), size=vector(0.1, 0.8, 0.5), color=color.white) 
+velocity = vector(0.05, 0, 0)
+t = 0
+dt = 0.05
+speed = 0.5
+amplitude = 1
+
+ball = sphere(pos=vector(0, 0, 0), radius=0.065)
 velocity = vector(0.01, 0, 0)
-
-box(pos=vector(-0.8, 0, 0), size=vector(0.1, 0.8, 0.5), color=color.white)
-box(pos=vector(0.8, 0, 0), size=vector(0.1, 0.8, 0.5), color=color.white)
-
-color_list = [color.red, color.orange, color.yellow, color.green, color.green, color.blue, color.purple] 
+color_list = [color.red, color.orange, color.yellow, color.green, color.blue, color.purple]
 color_index = 0
 
 while True:
-    rate(60)
+    rate(50)
+    bar1.pos.y = amplitude * sin(speed * t)
+    bar2.pos.y = amplitude * sin(speed * t)
+    t += dt
+    
     ball.pos = ball.pos + velocity
-   
     if ball.pos.x > 0.71 or ball.pos.x < -0.71:
         velocity.x = -velocity.x
     k = keysdown()
     if ' ' in k and (ball.pos.x > 0.71 or ball.pos.x < -0.71)  :
         ball.color = color_list[random.randint(0,2)]
+
